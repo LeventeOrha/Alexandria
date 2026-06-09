fb7ee6e5ada447c6d20aa54d70edc8ce7717d5bf
Levente Orha <leventeorha@gmail.com>
2026-06-09 11:23:09 +0200

    [UPD] Renaming from search.py

---
+++ src/alexandria/searchGoogle.py
@@ -0,0 +1,171 @@
+"""
+File to do a Google Books search
+"""
+
+import requests
+import sys
+from alexandria.utils import *
+from alexandria.data import *
+
+def sparseResults(response: dict):
+    """
+    Sparse the results of a Google Books search (too many info)
+
+    Parameters
+    ----------
+    response: `dict`
+        Response of a Google Books API request
+    
+    Returns
+    -------
+    books: `list`
+        Dictionary of each book, keys are the titles, with the following
+        data: title, author, publish date, image link, Google Books ID
+    """
+
+    books = []
+
+    for data in response["items"]:
+        book = {}
+        book["title"] = data["volumeInfo"].get("title")
+        book["author"] = data["volumeInfo"].get("authors")
+        book["date"] = data["volumeInfo"].get("publishedDate")
+        if "imageLinks" in data["volumeInfo"]:
+            book["img"] = data["volumeInfo"]["imageLinks"].get("thumbnail", None)
+        else:
+            book["img"] = None
+        book["ID"] = data["id"]
+        book["lang"] = data["volumeInfo"]["language"]
+
+        books.append(book)
+
+    return books
+
+def searchBook(title: str = "", author: str = "", lang: str = "en"):
+    """
+    Search a book on Google Books
+
+    Parameters
+    ----------
+    title: `str`
+        Title of the book
+    author: `str`
+        Author of the book
+    
+    Returns
+    -------
+    response: `list[dict]`
+        Response of the request, sparsed
+        Each element is a book
+    """
+    params = readSettings()
+    if title == '':
+        query = f"inauthor:{author}"
+    elif author == "":
+        query = f"intitle:{title}"
+    else:
+        query = f'intitle:{title}+inauthor:{author}'
+    url = "https://www.googleapis.com/books/v1/volumes"
+    params = {
+        "q": query,
+        "langRestrict": lang,
+        "maxResults": 5,
+        "key": params["GB_API"]
+    }
+
+    resp = requests.get(url, params=params).json()
+
+    if "items" in resp:
+        return sparseResults(resp)
+    else:
+        return None
+    
+def searchByID(ID: str):
+    """
+    Search a book by its unique volume ID on Google Books
+    """
+    params = readSettings()
+    url = f"https://www.googleapis.com/books/v1/volumes/{ID}"
+
+    params = {
+        "projection": "full",
+        "key": params["GB_API"]
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    return data
+
+def createBook(ID: str, shelf: str, start: str = "---", end: str = "---"):
+    info = searchByID(ID)
+
+    book = {}
+    book["title"] = info["volumeInfo"]["title"]
+    book["author"] = info["volumeInfo"]["authors"][0]
+    book["date"] = info["volumeInfo"]["publishedDate"]
+
+    # As the image sizes are, if alphabetically ordered, are decreasing with each key
+    # I rather keep the first one, theoretically the biggest
+    book["img"] = info["volumeInfo"]["imageLinks"][sorted(info["volumeInfo"]["imageLinks"].keys())[0]]
+
+    book["ID"] = ID
+    book["category"] = []
+    book["shelf"] = [shelf]
+
+    for category in info["volumeInfo"]["categories"]:
+        cat = category.split(" / ")[1]
+        if cat not in book["category"]:
+            book["category"].append(cat)
+
+    book["start"] = start
+    book["end"] = end
+
+    book = Book(**book)
+
+    return book
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        print(f"Useage: {sys.argv[0]} -t <title> -a <author> -l <language>")
+        exit()
+    
+    title = sys.argv[1]
+    author = sys.argv[2]
+    lang = sys.argv[3]
+
+    resp = searchBook(title, author, lang)
+
+    for i in range(len(resp)):
+        b = resp[i]
+        if b["lang"] != lang: # Ignoring books if (for some reason) wrong language results are returned
+            continue
+        print(f"{i+1}) {b["title"]} - {b["author"]} ({b["date"]}) - {b["img"]}")
+
+    pick = input("Which book is it actually? (c to cancel, or unique ID if another) ")  
+    if pick.lower() == "c":
+        exit()  
+    try:
+        pick = int(pick) - 1
+    except ValueError:
+        pass
+    cat = input("Which category? ")
+    start = input("Starting date: ")
+
+    try:
+        pick = int(pick) -1
+        book = createBook(resp[pick]["ID"], cat, start)
+    except ValueError:
+        book = createBook(pick, cat, start)
+
+    if bookExists(book.ID):
+        a = input(f"This book is already saved. Do you want to update the category and starting date? (y/n)")
+        if a == "y":
+            updateBook(book)
+    else:
+        addBooks([book])
+
+    print("Book succesfully added to database!")
+
+    exportCSV("book.csv")
