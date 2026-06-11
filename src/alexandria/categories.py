c3764110e714147212054f6463bbaed23c8c0db9
Levente Orha <leventeorha@gmail.com>
2026-06-11 10:19:31 +0200

    [ADD] translateCategories()

--- src/alexandria/categories.py
+++ src/alexandria/categories.py
@@ -17,3 +17,27 @@ def readCategories():
 
     return categories
 
+def translateCategories(cats: list, lang: str = "en"):
+    """
+    Translate a list of categories into what is in the database
+
+    Returns
+    -------
+    res: `list[str]`
+        Translated AND filtered categories
+    """
+    categories = readCategories()
+
+    if lang not in next(iter(categories.values())):
+        raise NotImplementedError(f"Language '{lang}' is not available yet. Sorry!")
+
+    res = []
+
+    cats[:] = [s.lower() for s in cats]
+
+    for key in categories:
+        if categories[key][lang].lower() in cats:
+            if key not in res: # Avoid double categories
+                res.append(key)
+    
+    return res
