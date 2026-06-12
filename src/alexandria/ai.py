c478e18d88fcd536458f89f9fc8e4fd11383ab30
Levente Orha <leventeorha@gmail.com>
2026-06-12 11:51:02 +0200

    [UPD] Consistent with new, class based packages

--- src/alexandria/ai.py
+++ src/alexandria/ai.py
@@ -1,16 +1,21 @@
 from google import genai
 from google.genai import types
 from dataclasses import asdict
+import yaml
 import alexandria.utils as u
-import alexandria.data as d
-import alexandria.searchGoogle as Google
-import alexandria.searchMoly as Moly
+from alexandria.data import Book, Database
+from alexandria.searchGoogle import Google
+from alexandria.searchMoly import Moly
 
 class AI:
-    def __init__(self, API_file: str, model: str, settings_file: str):
-        self.API_KEY = u.readSettings(API_file)["Gemini_API"]
+    def __init__(self, API_file: str, model: str, settings_file: str, db: Database):
+        API_keys = u.readSettings(API_file)
+        self.API_KEY = API_keys["Gemini_API"]
         self.settings = u.readSettings(settings_file)
         self.model = model
+        self.moly = Moly(db)
+        self.google = Google(API_keys["GB_API"])
+        self.db = db
 
         self.system_prompt = f"Role: \n{self.settings["system_prompt"]["role"]}\n"
         self.system_prompt += f"Rules: \n{"\n -".join(self.settings["system_prompt"]["rules"])}"
@@ -30,19 +35,30 @@ class AI:
             )
         )
 
+        self.conversation_log = []
+
         self.tools = {
-            "addBooks": d.addBooks,
-            "removeBook": d.removeBook,
-            "updateBook": d.updateBook,
-            "searchBy": d.searchBy,
-            "searchByCategory": d.searchByCategory,
-            "searchByShelf": d.searchByShelf,
-            "createBookGoogle": Google.createBook,
-            "searchByIDGoogle": Google.searchByID,
-            "createBookMoly": Moly.createBook,
-            "searchByIDMoly": Moly.searchById,
-            "getAllBooks": d.getAllBooks
+            "addBooks": self.db.addBooks,
+            "removeBook": self.db.removeBook,
+            "updateBook": self.db.updateBook,
+            "searchBy": self.db.searchBy,
+            "searchByCategory": self.db.searchByCategory,
+            "searchByShelf": self.db.searchByShelf,
+            "createBookGoogle": self.google.createBook,
+            "searchByIDGoogle": self.google.searchByID,
+            "createBookMoly": self.moly.createBook,
+            "searchByIDMoly": self.moly.searchByID,
+            "getAllBooks": db.getAllBooks
         }
+
+    def saveChat(self, filename: str):
+        """
+        Save the conversation
+        """
+        with open(filename, "wt", encoding="utf-8") as out:
+            yaml.safe_dump(self.conversation_log, out, sort_keys=False, allow_unicode=True)
+        return {"success": True}
+
 
     def generateResponse(self, user_prompt: str):
         """
@@ -50,20 +66,23 @@ class AI:
         """
         response = self.chat.send_message(user_prompt)
 
-        # Check out if Gemini want to call a function (or several)
+        self.conversation_log.append({"role": "user", "content": user_prompt})
+
+        # Check out if Gemini wants to call a function (or several)
         while True:
 
             part = response.candidates[0].content.parts[0]
 
             if not getattr(part, "function_call", None):
+                self.conversation_log.append({"role": "assistant", "content": response.text})
                 return response.text
 
             call = part.function_call
 
             if call.name in ["removeBook", "updateBook"]:
-                result = self.tools[call.name](d.Book(**call.args))
+                result = self.tools[call.name](Book(**call.args))
             elif call.name == "addBooks":
-                result = self.tools[call.name]([d.Book(**b) for b in call.args["books"]])
+                result = self.tools[call.name]([Book(**b) for b in call.args["books"]])
             else:
                 result = self.tools[call.name](**call.args)
 
@@ -76,6 +95,8 @@ class AI:
             if result is None:
                 result = {"success": True}
 
+            self.conversation_log.append({"role": "tool call", "name": call.name, "args": dict(call.args), "response": result})
+
             tool_response = types.Part.from_function_response(
                 name=call.name,
                 response=result
@@ -84,10 +105,12 @@ class AI:
             response = self.chat.send_message([tool_response])
 
 if __name__ == "__main__":
+    import traceback
     params = u.readSettings()
-    gemini = AI(params["API_file"], params["Gemini_model"], params["AI_settings"])
+    db = Database(params["datafile"])
+    gemini = AI(params["API_file"], params["Gemini_model"], params["AI_settings"], db)
 
-    print("Welcome! Type /exit to end chat.")
+    print("Welcome! Type /help to display guide.")
 
     while True:
         user_prompt = input("> ")
@@ -95,10 +118,30 @@ if __name__ == "__main__":
         if user_prompt.strip().lower() == "/exit":
             print("We had a nice chat! Good bye!")
             break
+
+        if user_prompt.strip().lower() == "/help":
+            print("Help requested. Here are the commands you may use: ")
+            print("- /help - Display this guide")
+            print("- /exit - End chat and close AI assistant")
+            print("- /save <filename> - Save conversation into 'filename'")
+            continue
+
+        if "/save" in user_prompt.strip().lower():
+            gemini.saveChat(user_prompt.split(" ")[1])
+            continue
         
         try:
             response = gemini.generateResponse(user_prompt)
             print(response)
         except genai.errors.ServerError:
             print("Sorry, I don't feel like it today. Come back tomorrow!")
+            if input("Do you want to save the chat log? (y/n) ") == "y":
+                gemini.saveChat("gemini_chat_log.yaml")
+            break
+        except Exception as e:
+            print("Something else went wrong. Check the error please!")
+            if input("Do you want to save the chat log? (y/n) ") == "y":
+                gemini.saveChat("gemini_chat_log.yaml")
+            print(e.__repr__())
+            traceback.print_exc()
             break
