93301d990d141d86cf37fbb986591d092cc1df0d
Levente Orha <leventeorha@gmail.com>
2026-06-12 10:44:50 +0200

    [UPD] Class based now

--- src/alexandria/data.py
+++ src/alexandria/data.py
@@ -2,11 +2,9 @@ from dataclasses import dataclass, asdict
 from typing import List
 import sqlite3
 import json
-from alexandria import utils as u
+import os
 
-# Default value for the 
-DATAFILE = u.readSettings()["datafile"]
-
+# Book class to hold everything together
 @dataclass
 class Book:
     title: str
@@ -34,290 +32,242 @@ class Book:
         s += f"\tstart: {self.start}\n"
         s += f"\tend: {self.end}\n"
         return s
+    
+# Database class, to not open connection again and again
+class Database:
 
+    def __init__(self, datafile: str):
+        if os.path.exists(datafile) == False:
+            raise FileNotFoundError(f"File {datafile} does not exist. Please, check it!")
+        self.filename = datafile
+        self.conn = sqlite3.connect(datafile)
+        self.conn.row_factory = sqlite3.Row
+        self.cur = self.conn.cursor()
 
-def get_connection():
-    """
-    Create SQL connection to datafile (books.db)
-    """
-    conn = sqlite3.connect(DATAFILE)
-    conn.row_factory = sqlite3.Row
-    return conn
+    def createDatabase(self):
+        """
+        Create an empty database with these keys
+        """
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS books (
+            ID TEXT PRIMARY KEY,
+            title TEXT,
+            author TEXT,
+            date TEXT,
+            img TEXT,
+            shelf TEXT,
+            category TEXT,
+            start TEXT,
+            end TEXT
+        )
+        """)
+        self.conn.commit()
+        return {"success": True}
+    
+    def close(self):
+        """
+        Close the connection
+        """
+        self.conn.close()
+        return {"success": True}
+    
+    def rowToBook(self, row):
+        """
+        Convert SQLite row to Book object
+        """
+        data = dict(row)
 
+        data["shelf"] = json.loads(data["shelf"])
+        data["category"] = json.loads(data["category"])
 
-def createDatabase():
-    """
-    Create database table
-    """
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS books (
-        ID TEXT PRIMARY KEY,
-        title TEXT,
-        author TEXT,
-        date TEXT,
-        img TEXT,
-        shelf TEXT,
-        category TEXT,
-        start TEXT,
-        end TEXT
-    )
-    """)
-
-    conn.commit()
-    conn.close()
-
-
-def row_to_book(row):
-    """
-    Convert SQLite row to Book object
-    """
-    data = dict(row)
-
-    data["shelf"] = json.loads(data["shelf"])
-    data["category"] = json.loads(data["category"])
-
-    return Book(**data)
-
-
-def addBooks(books: list[Book]):
-    """
-    Add many books to the database
-    """
-    conn = get_connection()
-    cur = conn.cursor()
-
-    data = []
-
-    for book in books:
-        data.append((
-            book.ID,
-            book.title,
-            book.author,
-            book.date,
-            book.img,
-            json.dumps(book.shelf),
-            json.dumps(book.category),
-            book.start,
-            book.end
-        ))
-
-    cur.executemany("""
-    INSERT OR IGNORE INTO books
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, data)
-
-    conn.commit()
-    conn.close()
-
-
-def updateBook(book: Book):
-    """
-    Update the data of one book
-    """
-    data = asdict(book)
-
-    data["shelf"] = json.dumps(book.shelf)
-    data["category"] = json.dumps(book.category)
-
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-    UPDATE books
-    SET
-        title = :title,
-        author = :author,
-        date = :date,
-        img = :img,
-        shelf = :shelf,
-        category = :category,
-        start = :start,
-        end = :end
-    WHERE ID = :ID
-    """, data)
-
-    conn.commit()
-    conn.close()
-
-
-def removeBook(book: Book):
-    """
-    Remove a book from the database
-    """
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-    DELETE FROM books
-    WHERE ID = ?
-    """, (book.ID,))
-
-    conn.commit()
-    conn.close()
-
-
-def searchBy(key: str, value: str):
-    """
-    Search all books by a specific key/value pair
-
-    Parameters
-    ----------
-    key: str
-        Column name
-    value: str
-        Value to search for
-
-    Returns
-    -------
-    list[Book]
-    """
-    conn = get_connection()
-    cur = conn.cursor()
-
-    allowed_columns = {
-        "ID",
-        "title",
-        "author",
-        "date",
-        "img",
-        "start",
-        "end"
-    }
-
-    if key not in allowed_columns:
-        raise ValueError("Invalid column name")
-
-    cur.execute(
-        f"SELECT * FROM books WHERE {key} = ?",
-        (value,)
-    )
-
-    rows = cur.fetchall()
-
-    conn.close()
-
-    return [row_to_book(row) for row in rows]
-
-
-def searchByCategory(category: str):
-    """
-    Search books containing a category
-    """
-    books = getAllBooks()
-
-    return [
-        book for book in books
-        if category in book.category
-    ]
-
-
-def searchByShelf(shelf: str):
-    """
-    Search books containing a shelf
-    """
-    books = getAllBooks()
-
-    return [
-        book for book in books
-        if shelf in book.shelf
-    ]
-
-
-def getAllBooks():
-    """
-    Get all books from database
-    """
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("SELECT * FROM books")
-
-    rows = cur.fetchall()
-
-    conn.close()
-
-    return [row_to_book(row) for row in rows]
-
-def exportCSV(file: str):
-    """
-    Write all books from the database to a csv file
-    """
-    books = getAllBooks()
-    with open(file, "wt", encoding="utf-8") as out:
+        return Book(**data)
+    
+    def addBooks(self, books: list[Book]):
+        """
+        Add many books to the database
+        """
+        data = []
         for book in books:
-            s = f"{book.ID};{book.title};{book.author};{book.date};{book.start};{book.end};"
-            s += f"{", ".join(book.shelf)};"
-            s += f"{", ".join(book.category)};"
-            s += f"{book.img}\n"
-            out.write(s)
-    return 
+            data.append((book.ID, book.title, book.author, book.date, book.img,
+                         json.dumps(book.shelf), json.dumps(book.category), book.start, book.end))
+            
+        self.cur.executemany("""
+        INSERT OR IGNORE INTO books
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, data)
 
-def exportYML(file: str):
-    """
-    Write all books from the database to a YAML file (readibilty, debugging)
-    """
-    import yaml
-    books = getAllBooks()
-    with open(file, "wt", encoding="utf-8") as out:
-        yaml.safe_dump([asdict(book) for book in books], out, sort_keys=False, allow_unicode=True)
-    return
+        self.conn.commit()
+        return {"success": True}
+    
+    def updateBook(self, book: Book):
+        """
+        Update data of one book, based on ID
+        """
+        data = asdict(book)
+        data["shelf"] = json.dumps(book.shelf)
+        data["category"] = json.dumps(book.category)
 
-def bookExists(book_id: str) -> bool:
-    """
-    Check if a book (by ID) exists in the database
-    """
-    conn = get_connection()
-    cur = conn.cursor()
+        self.cur.execute("""
+        UPDATE books
+        SET
+            title = :title,
+            author = :author,
+            date = :date,
+            img = :img,
+            shelf = :shelf,
+            category = :category,
+            start = :start,
+            end = :end
+        WHERE ID = :ID
+        """, data)
 
-    cur.execute(
-        "SELECT 1 FROM books WHERE ID = ? LIMIT 1",
-        (book_id,)
-    )
+        self.conn.commit()
+        return {"success": True}
+    
+    def removeBook(self, ID: str):
+        """
+        Remove book with given ID
+        """
+        self.cur.execute("""
+        DELETE FROM books
+        WHERE ID = ?
+        """, (ID,))
 
-    exists = cur.fetchone() is not None
+        self.conn.commit()
+        return {"success": True}
+    
+    def searchBy(self, key: str, value: str) -> list[Book]:
+        """
+        Search all books by a specific key/value pair
 
-    conn.close()
+        Parameters
+        ----------
+        key: str
+            Column name
+        value: str
+            Value to search for
 
-    return exists         
+        Returns
+        -------
+        list[Book]
+        """
+        allowed_columns = [
+            "ID", "title", "author", "date",
+            "img", "start", "end"
+        ]
 
-def importCSV(file: str):
-    """
-    From a csv file, import books into the program and save it to the database
-    """
-    with open(file, "rt", encoding="utf-8") as inp:
-        books = []
-        for line in inp:
-            book = {}
-            parts = line.strip().split(";")
-            book["ID"] = parts[0]
-            book["title"] = parts[1]
-            book["author"] = parts[2]
-            book["date"] = parts[3]
-            book["start"] = parts[4]
-            book["end"] = parts[5]
-            book["shelf"] = parts[6].split(", ")
-            book["category"] = parts[7].split(", ")
-            book["img"] = parts[8]
-            books.append(Book(**book))
-
-    for book in books:
-        if bookExists(book.ID):
-            updateBook(book)
-        else:
-            addBooks([book])
+        if key not in allowed_columns:
+            raise ValueError(f"Invalid column name - {key}")
         
-    return books
+        self.cur.execute(
+            f"SELECT * FROM books WHERE LOWER({key}) LIKE LOWER(?)",
+            (f"%{value}%",)
+        )
 
+        rows = self.cur.fetchall()
 
+        return [self.rowToBook(row) for row in rows]
 
+    def getAllBooks(self) -> list[Book]:
+        """
+        Get all books from the database
+        """
+        self.cur.execute("SELECT * FROM books")
+
+        rows = self.cur.fetchall()
+
+        return [self.rowToBook(row) for row in rows]
+    
+    def searchByCategory(self, category: str) -> list[Book]:
+        """
+        Search books by a category
+        """
+        books = self.getAllBooks()
+
+        res = []
+        for book in books:
+            if category in book.category:
+                res.append(book)
+        
+        return res
+    
+    def searchByShelf(self, shelf: str) -> list[Book]:
+        """
+        Search books by a shelf
+        """
+        books = self.getAllBooks()
+
+        res = []
+        for book in books:
+            if shelf in book.shelf:
+                res.append(book)
+        
+        return res
+    
+    def exportCSV(self, filename: str):
+        """
+        Write all books from the database to a csv file
+        """
+        books =self.getAllBooks()
+        with open(filename, "wt", encoding="utf-8") as out:
+            for book in books:
+                s = f"{book.ID};{book.title};{book.author};{book.date};{book.start};{book.end};"
+                s += f"{", ".join(book.shelf)};"
+                s += f"{", ".join(book.category)};"
+                s += f"{book.img}\n"
+                out.write(s)
+        return {"success": True}
+    
+    def exportYML(self, filename: str):
+        """
+        Write all books from the database to a YAML file (readibilty, debugging)
+        """
+        import yaml
+        books = self.getAllBooks()
+        with open(filename, "wt", encoding="utf-8") as out:
+            yaml.safe_dump([asdict(book) for book in books], out, sort_keys=False, allow_unicode=True)
+        return {"success": True}
+    
+    def bookExists(self, ID: str) -> bool:
+        """
+        Check if book with ID exists
+        """
+        self.cur.execute("SELECT 1 FROM books WHERE ID = ? LIMIT 1", (ID,))
+        exists = self.cur.fetchone() is not None
+        return exists
+    
+    def importCSV(self, filename: str):
+        """
+        From a csv file, import books into the database
+        Update them if existing, adding if ID not exist
+        """
+        with open(filename, "rt", encoding="utf-8") as inp:
+            books = []
+            for line in inp:
+                book = {}
+                parts = line.strip().split(";")
+                book["ID"] = parts[0]
+                book["title"] = parts[1]
+                book["author"] = parts[2]
+                book["date"] = parts[3]
+                book["start"] = parts[4]
+                book["end"] = parts[5]
+                book["shelf"] = parts[6].split(", ")
+                book["category"] = parts[7].split(", ")
+                book["img"] = parts[8]
+                books.append(Book(**book))
+
+        new_books = []
+        for book in books:
+            if self.bookExists(book.ID):
+                self.updateBook(book)
+            else:
+                new_books.append(book)
+        self.addBooks(new_books)
+            
+        return books
+    
 if __name__ == "__main__":
-    import os
-    if os.path.exists(DATAFILE):
-        exportYML("books.csv")
-    else:
-        createDatabase()
-        importCSV("books.csv")
-        print(f"{len(getAllBooks())} books successfully imported!")
+    db = Database("../../datafiles/books.db")
+    db.exportYML("books.yaml")
+    db.close()
