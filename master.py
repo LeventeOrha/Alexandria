8f52c571815a551373dc4bf83a3bb10521d46092
Levente Orha <leventeorha@gmail.com>
2026-06-16 14:47:55 +0200

    [ADD] GUI useage option, error

--- master.py
+++ master.py
@@ -2,5 +2,9 @@ from ui.cmd import CMD
 import alexandria.utils as u
 
 params = u.readSettings()
+if params["GUI_useage"]:
+    params["GUI_useage"] = False
+    u.writeSettings(params)
+    raise NotImplementedError("Graphical user interface not implemented yet. Defaulting to terminal useage.")
 cmd = CMD(params)
 cmd.main()
