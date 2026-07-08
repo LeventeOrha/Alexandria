9572a564bf6f5d7cd9b03f55afcb21f4d3d44355
Levente Orha <leventeorha@gmail.com>
2026-07-08 14:09:14 +0200

    [ADD] saveBook() and UI modification

--- src/ui/buttons.js
+++ src/ui/buttons.js
@@ -1,19 +1,11 @@
+// Shortcutting the API
+const python = new Python()
+
+// Get available shelves and categories
+const shelfOptions = python.getShelves()
+const categoryOptions = python.getCategories()
+
 // All specific button functions
-
-// Get shelves and categories
-// TODO - change code here for actual shelves and categories from the database
-const shelfOptions = [
-    "Reading",
-    "Read",
-    "To read"
-];
-
-const categoryOptions = [
-    "Fiction",
-    "Science",
-    "History",
-    "Biography"
-];
 
 // Edit and save mode for a book's details
 const editbtn = document.getElementById("editBookDetails")
@@ -78,21 +70,75 @@ function editBook() {
     const startDate = document.getElementById("start")
     // TODO - change it here to actual date parsing and not this "quick" solution
     let date = startDate.textContent.split(" ")[1]
-    startDate.innerHTML = `Start: <input type="date" value="${date}" class="dateTag">`
+    startDate.innerHTML = `Start: <input type="date" value="${date}" class="dateTag" id="startDate">`
 
     // Change end to input
     const endDate = document.getElementById("end")
     date = endDate.textContent.split(" ")[1]
     if (date == "---"){
-        endDate.innerHTML = `End: <input type="date" class="dateTag">`
+        endDate.innerHTML = `End: <input type="date" class="dateTag" id="endDate">`
     }
     else {
-        endDate.innerHTML = `End: <input type="date" value="${date}" class="dateTag">`
+        endDate.innerHTML = `End: <input type="date" value="${date}" class="dateTag" id="endDate">`
     }
 }
 
 // Save mode
 function saveBook() {
+    // Get the book's all data from the database
+    let book = python.searchByID(document.querySelector(".bookData").id)
+
+    // Remove "removeTag" buttons
+    let removes = document.querySelectorAll(".removeTag")
+    removes.forEach((element) => {element.remove()})
+
+    // Remove "addTag" buttons
+    removes = document.querySelectorAll(".addTag")
+    removes.forEach((element) => {element.remove()})
+
+    // And now update the fields
+    // First the categories
+    let categories = document.getElementById("categoryTags")
+    categories = [...categories.querySelectorAll(".tag")].map(el =>
+        el.matches("span") ? el.textContent.trim() : el.value
+    )
+    book["category"] = categories
+
+    // Now the shelves
+    let shelf = document.getElementById("shelfTags")
+    shelf = [...shelf.querySelectorAll(".tag")].map(el =>
+        el.matches("span") ? el.textContent.trim() : el.value
+    )
+    book["shelf"] = shelf
+
+    // Starting date
+    book["start"] = document.getElementById("startDate").value
+
+    // End date
+    book["end"] = document.getElementById("endDate").value
+
+    // Save the updated book
+    python.saveBook(book)
+
+    console.log(book)
+
+    // Replace existing select/options with its value
+    let selects = document.querySelectorAll("select.tag")
+    selects.forEach((select) => {
+        const tag = document.createElement("span")
+        tag.className = "tag"
+
+        tag.innerHTML = `<span>${select.options[select.selectedIndex].text}</span>`
+
+        select.replaceWith(tag)
+    })
+
+    // Replace all date inputs with the values
+    document.querySelectorAll('input.dateTag[type="date"]').forEach(input => {
+        const span = document.createElement('span');
+        span.textContent = input.value || '---';
+        input.replaceWith(span);
+    });
 
 }
 
