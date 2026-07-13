20bfe633588fb906eb31804ecd75089fcdf7a709
Levente Orha <leventeorha@gmail.com>
2026-07-13 12:06:58 +0200

    [ADD] Message sending

--- src/ui/buttons.js
+++ src/ui/buttons.js
@@ -413,3 +413,48 @@ document.addEventListener("change", function (event) {
     input.focus();
   }
 });
+
+// Send and get new message in AI chat
+function createNewMessage(content, sender) {
+    // Create a HTML div to straight put in
+    // sender MUST BE "ai" or "user"
+
+    // Rewrite message to HTML
+    const htmlMessage = marked.parse(content)
+
+    // Create the holder
+    const message = document.createElement("div")
+    message.className = `${sender}Message`
+
+    // Create the inner span
+    const span = document.createElement("span")
+    span.className = `${sender}MessageText`
+
+    // Put in text
+    span.innerHTML = htmlMessage
+
+    // Wrap it
+    message.appendChild(span)
+
+    // Put it out
+    document.getElementById("aiBody").appendChild(message)
+}
+
+const sendMessage = document.getElementById("sendNewMessage")
+sendMessage.addEventListener('click', () => {
+    // Get the new text from the user
+    const textarea = document.getElementById("writeNewMessage")
+    var userMessage = textarea.value
+
+    // Clear out text area
+    textarea.value = ""
+
+    // Put out the user message
+    createNewMessage(userMessage, "user")
+
+    // Get the answer
+    const answer = python.getAIMessage(userMessage)
+
+    // Put out the answer
+    createNewMessage(answer, "ai")
+})
