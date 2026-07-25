68389494101e2b14c7511b55d11c5671a5498d72
Levente Orha <leventeorha@gmail.com>
2026-07-25 14:51:17 +0200

    [ADD] GUI useage option, only as debug (-d)

--- master.py
+++ master.py
@@ -1,10 +1,16 @@
 from ui.cmd import CMD
+from ui.gui import main
+import sys
 import alexandria.utils as u
 
 params = u.readSettings()
 if params["GUI_useage"]:
-    params["GUI_useage"] = False
-    u.writeSettings(params)
-    raise NotImplementedError("Graphical user interface not implemented yet. Defaulting to terminal useage.")
-cmd = CMD(params)
-cmd.main()
+    if "-d" in sys.argv: # Debugging "mode"
+        main(params)
+    else:
+        params["GUI_useage"] = False
+        u.writeSettings(params)
+        raise NotImplementedError("Graphical user interface not implemented yet. Defaulting to terminal useage.")
+else:
+    cmd = CMD(params)
+    cmd.main()
