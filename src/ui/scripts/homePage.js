ab3e9c4653aefd7e78d58e002cf1fb00355c9c99
Levente Orha <leventeorha@gmail.com>
2026-07-31 16:36:50 +0200

    [ADD] Reading history calendar

--- src/ui/scripts/homePage.js
+++ src/ui/scripts/homePage.js
@@ -92,3 +92,50 @@ document.getElementById("currentReading").addEventListener("click", (event) => {
     document.getElementById("searchButton").click()
     document.getElementById("startSearch").click()
 })
+
+// Get the most dense color of an image at imgLink
+async function getColor(imgLink) {
+
+    // Prefer a strong UI color
+    const color = "#16e654"
+
+    return color;
+}
+
+function addOneDay(dateString) {
+    const d = new Date(dateString);
+    d.setDate(d.getDate() + 1);
+    return d.toISOString().split("T")[0];
+}
+
+async function createCalender() {
+    const books = await window.python.getHistory()
+    console.log(books)
+
+    const events = await Promise.all(
+        books.map(async item => {
+            const color = await getColor(item.img);
+
+            return {
+                title: item.title,
+                start: item.start,
+                end: addOneDay(item.end),
+                allDay: true,
+                backgroundColor: color,
+                borderColor: color,
+                textColor: "white"
+            };
+        })
+    );
+
+    const calendar = new FullCalendar.Calendar(
+        document.getElementById("calendar"),
+        {
+            initialView: "dayGridMonth",
+            height: "auto",
+            events
+        }
+    )
+    calendar.render()
+}
+createCalender()
