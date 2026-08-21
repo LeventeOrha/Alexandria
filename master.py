72d151a122794e408113099b2963a5a1ae59bdb4
Levente Orha <leventeorha@gmail.com>
2026-08-21 14:33:04 +0200

    [MOD] Debug mode ignores gui settings

--- master.py
+++ master.py
@@ -4,7 +4,7 @@ import sys
 import alexandria.utils as u
 
 params = u.readSettings()
-if params["GUI_useage"]:
+if (params["GUI_useage"]) and ("-d" not in sys.argv):
     params["GUI_useage"] = False
     u.writeSettings(params)
     raise NotImplementedError("Graphical user interface not implemented yet. Defaulting to terminal useage.")
